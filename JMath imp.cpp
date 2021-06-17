6ff55a398e214b6b48f93653860bd8c46d0459a9
200MB <81110694+200MB@users.noreply.github.com>
2021-06-17 21:06:15 +0400

    added more stuff

--- JMath imp.cpp
+++ JMath imp.cpp
@@ -42,7 +42,7 @@ double JMath::ArithmeticProg(int n, double a1, double d) {
 		return a1 + (d * (n - 1));
 	}
 	else {
-		cout << "Error(ArithmeticProg) Number of terms is negative";
+		cout << "Critical Warning(ArithmeticProg) Number of terms is negative";
 	}
 }
 double JMath::ArithmeticProgSum(int n, double a1, double d) {
@@ -50,7 +50,7 @@ double JMath::ArithmeticProgSum(int n, double a1, double d) {
 		return ((2 * a1 + d * (n - 1)) / 2) * n;
 	}
 	else {
-		cout << "Error(ArithmeticProgSum) Number of terms is negative";
+		cout << "Critical Warning(ArithmeticProgSum) Number of terms is negative";
 	}
 }
 double JMath::GeometricProg(int n, double b1, double q) {
@@ -58,7 +58,7 @@ double JMath::GeometricProg(int n, double b1, double q) {
 		return b1 * pow(q, n - 1);
 	}
 	else {
-		cout << "Error(ArithmeticProgSum) Number of terms is negative";
+		cout << "Critical Warning(ArithmeticProgSum) Number of terms is negative";
 	}
 }
 double JMath::GeometricProgMult(int n, double b1, double q) {
@@ -66,12 +66,12 @@ double JMath::GeometricProgMult(int n, double b1, double q) {
 		return (b1 * (pow(q, n) - 1)) / (q - 1);
 	}
 	else {
-		cout << "Error(ArithmeticProgSum) Number of terms is negative";
+		cout << "Critical Warning(ArithmeticProgSum) Number of terms is negative";
 	}
 }
 string JMath::JLog(long double n, long double n1, int loopcount) {
 	if (n < 0 || n1 < 0 || loopcount < 0) {
-		return "Error(JLog) Can't take negative Integers";
+		return "Critical Error(JLog) Can't take negative Integers";
 	}
 	long double n1dup = n1;
 	string number = "";
@@ -109,6 +109,10 @@ int JMath::CountDigit(int number) {
 	return int(log10(number) + 1);
 }
 vector<int> JMath::FindMostFrequent(vector<int>v) {
+	if (v.size() == 0) {
+		cout << "Fatal Error(FindLeastFrequent)-vector is empty" << endl;
+		exit(EXIT_FAILURE);
+	}
 	vector<int>v1;
 	sort(v.begin(), v.end());
 	int max = 1, most = v[0], current = 1;
@@ -167,9 +171,9 @@ vector<int> JMath::FindMostFrequent(vector<int>v) {
 		}
 		else if (v[i] != v[i - 1] && i + 1 != v.size()) {
 			if (current == max) {
+				v1.push_back(v[i-1]);
 				sort(v1.begin(), v1.end());
 				v1.erase(unique(v1.begin(), v1.end()), v1.end());
-				v1.push_back(v[i-1]);
 				current = 1;
 			}
 			else if (current < max) {
@@ -178,6 +182,116 @@ vector<int> JMath::FindMostFrequent(vector<int>v) {
 		}
 	}
 }
+vector<int> JMath::FindLeastFrequent(vector<int>v){
+	if (v.size() == 0) {
+		cout << "Fatal Error(FindLeastFrequent)-vector is empty" << endl;
+		exit(EXIT_FAILURE);
+	}
+	vector<int>v1;
+	sort(v.begin(), v.end());
+	int least = INT_MAX, current = 1;
+	for (int i = 1; i < v.size(); i++) {
+		if (i + 1 == v.size() && v[i] == v[i - 1]) {
+			current++;
+			if (current < least)
+			{
+				least = current;
+				break;
+			}
+		}
+		else if (i + 1 == v.size() && v[i] != v[i - 1]) {
+			least = 1;
+		}
+
+
+		else if (v[i] == v[i - 1]) {
+			current++;
+		}
+		else if (v[i] != v[i - 1] && i + 1 != v.size()) {
+			if (current < least) {
+				least = current;
+				current = 1;
+			}
+			else {
+				current = 1;
+			}
+		}
+	}
+	current = 1;
+	for (int i = 1; i < v.size(); i++) {
+		if (i + 1 == v.size() && v[i] == v[i - 1]) {
+			current++;
+			if (current == least) {
+
+				v1.push_back(v[i - 1]);
+				sort(v1.begin(), v1.end());
+				v1.erase(unique(v1.begin(), v1.end()), v1.end());
+				return v1;
+				break;
+			}
+			else {
+				return v1;
+				break;
+			}
+
+
+		}
+		if (i + 1 == v.size() && v[i] != v[i - 1]) {
+			if (current == least) {
+				v1.push_back(v[i]);
+				sort(v1.begin(), v1.end());
+				v1.erase(unique(v1.begin(), v1.end()), v1.end());
+				return v1;
+				break;
+			}
+			else {
+				return v1;
+				break;
+			}
+		}
+		if (v[i] == v[i - 1]) {
+			current++;
+		}
+		else if (v[i] != v[i - 1] && i + 1 != v.size()) {
+			if (current == least) {
+				v1.push_back(v[i - 1]);
+				sort(v1.begin(), v1.end());
+				v1.erase(unique(v1.begin(), v1.end()), v1.end());
+				current = 1;
+			}
+			else {
+				current = 1;
+			}
+		}
+	}
+}
+string JMath::ReverseAstring(string pop) {
+	if (pop.size() == 0) {
+		cout << "Warning(ReverseAstring)-String is empty" << endl;
+	}
+	string s = "";
+	for (int i = pop.size() - 1; i >= 0; i--) {
+		s += pop[i];
+	}
+	return s;
+}
+int JMath::AtoBThSystem(int a, int b) {
+	if (a == 0) {
+		cout << "Fatal Error(errorAtoBthSystem)-Error 0 is impossible to convert" << endl;
+		exit(EXIT_FAILURE);
+	}
+	string s = "";
+	while (a > 0) {
+		s += to_string(a % b);
+		a /= b;
+	}
+	return stoi(ReverseAstring(s));
+}
+
+
+
+
+
 		
 
 
